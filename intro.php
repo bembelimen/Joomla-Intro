1cb51527e2b8039e7e3cb80b17b054f858c6507f
Helrunar <info@projektwaterkant.de>
2012-09-19 11:10:27 +0200

    translate the comment to english

--- intro.php
+++ intro.php
@@ -8,39 +8,34 @@ class plgSystemIntro extends JPlugin {
 
 	// constructor
 	function plgSystemIntro(&$subject, $params) {
-
-		// ruft unseren base constructer auf.
         parent::__construct($subject, $params);
     }
 
 
-    // OnAfterDispatch event wird direkt von joomla aufgerufen
-    // und wird noch vor dem rendern der seite ausgeführt, dort setzen wir die intro seite.
+    // OnAfterDispatch event to render the introsite.
     function OnAfterDispatch()
 	{
         $session = JFactory::getSession();
 
-		//fragt ab, ob wir uns im frontend befinden, ansonsten würde die intro seite auch im backend geschaltet.
+		//check for frontend
 		if (!JFactory::getApplication()->isSite()) return ;
 
-		// ganz simpel, es wird gefragt, ob in der session der wert "intro"
-		// enthalten ist, wenn nicht, wird die intro seite geschaltet.
+		// check for intro into the session
        	$intro = $session->get('intro');
 
        	if(empty($intro))
        	{
-       		// die plugin parameter (also das html für die intro seite)werden abgerufen und ausgegeben.
+       		// get the parameter with the html
        		echo $this->params->get('intro');
 
-       		// es wird in die session der wert intro gesetzt,
-			// damit die vorschalt seite kein zweites mal ausgegeben wird.
+       		// set intro to session
        		$session->set('intro', 1);
 
-       		// alles weitere wird abgebrochen, damit die joomla seite nicht gerendert wird und nur das html ausgegeben wird.
+       		// stop rendering the site and show the html-output
        		exit;
        	}
 
-       	// wenn der parameter auf 1 steht wird die session am leben gehalten, solange das browser fenster geöffnet ist.
+       	// keep the session alive
        	if($this->params->get('session'))
        	{
        		echo JHTML::_('behavior.keepalive');
